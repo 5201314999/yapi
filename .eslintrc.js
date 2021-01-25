404355d2165e52c3af264e7064cdc464dd2536be
5201314999 <1446418561@qq.com>
2021-01-25 20:14:53 +0800

    chore: custom-yapi init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,29 @@
 module.exports = {
     env: {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "node": true
+        browser: true,
+        commonjs: true,
+        es6: true,
+        node: true,
     },
-    extends: ["eslint:recommended", "plugin:react/recommended"],
-    parser: "babel-eslint",
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
     parserOptions: {
-        "ecmaFeatures": {
-            "jsx": true
+        ecmaFeatures: {
+            jsx: true,
         },
-        "sourceType": "module"
+        sourceType: 'module',
     },
-    plugins: [
-        "react",
-        "import"
-    ],
+    plugins: ['react', 'import'],
     rules: {
-        "indent": ["off", 2],
-        "react/display-name": ["off"],
-        "react/jsx-indent": ["error", 2],
-        "comma-dangle": ["error", "never"],
-        "no-console": ["off"],
-        "import/no-unresolved": ["off"],
-        "react/no-find-dom-node": ["off"],
-        "no-empty": ["off"]
+        indent: ['off', 2],
+        'react/display-name': ['off'],
+        'react/jsx-indent': ['error', 2],
+        'comma-dangle': ['error', 'never'],
+        'no-console': ['off'],
+        'import/no-unresolved': ['off'],
+        'react/no-find-dom-node': ['off'],
+        'no-empty': ['off'],
         // "react/no-unescaped-entities": 0
-    }
+        'react/no-deprecated': ['off'],
+    },
 };
-
